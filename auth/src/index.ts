3ae955e4e5f4d0e94d60bc9b4ee690ed4e43b33f
zayn-m <zain.hassan1212@gmail.com>
2021-07-30 14:59:05 +0500

    initial commit

---
+++ auth/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error';
+
+const app = express();
+app.use(json());
+
+app.get('/', (req, res) => {
+  res.send({ status: 'ok' });
+});
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.use(errorHandler);
+
+app.listen(3000, () => console.log(`Listening on port 3000`));
